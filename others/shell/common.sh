be2dca43af9cd6b7e5e0fc71f506fbc3022996cc
royalsong <royalsong@futunn.com>
2023-09-28 17:43:49 +0800

    add git commands

--- others/shell/common.sh
+++ others/shell/common.sh
@@ -3,28 +3,22 @@
 # generate key pair and copy to remote server.
 ssh-keygen -t rsa
 ssh-copy-id root@1.2.3.4
-
 # login
 ssh user@1.2.3.4
-
 # scp src dst
 scp test.txt  root@1.2.3.4:/root
 scp root@1.2.3.4:/root/test.txt .
 
-
 ## ln
 ln -s target_file link_file
-
 # change target of link_file
 ln -snf target_file link_file
-
 
 ## netstat
 # install
 apt install net-tools
 
 netstat -lnap
-
 # list all network adapters
 netstat -i
 
@@ -32,33 +26,26 @@ netstat -i
 # https://computingforgeeks.com/how-to-check-tcp-connections-states-in-linux-with-netstat/
 netstat -nat | awk '{print $6}' | sort | uniq -c | sort -r
 
-
 ## lsof. list open file
 # https://www.cnblogs.com/muchengnanfeng/p/9554993.html
 # search by port
 lsof -i:1234
-
 # search by process id
 lsof -p 1234
-
 
 ####################### Text Processing #################
 ## sort. 对文件中所有行，默认字典序排序. -n 按数值排序；-k 取某一列排序；-t 指定列的分隔符；-r 倒序；
 sort -n -k 5 -t " " text.txt
 
-
 ## wc
 # count by line
 wc -l text.txt
-
 # count by word
 wc -w text.txt
-
 
 ##uniq
 # group by line and count the number of same line.
 uniq -c text.txt
-
 
 ## awk
 # 参考 菜鸟教程
@@ -89,17 +76,15 @@ df -hT
 ##du
 # 查看当前文件夹下各个文件占用空间大小
 du -h --max-depth=1 ./*
-
 # 查看该文件夹大小
 du -hs [path]
 
 # network traffic analysis
 # https://www.scaler.com/topics/linux-monitor-network-traffic/
 cat /proc/net/dev
-ifconfig
 
+ifconfig
 # show realtime traffic for each program
 nethogs
-
 # show realtime traffic for each tcp connection
 iftop
--- others/shell/git.sh
+++ others/shell/git.sh
@@ -1,37 +1,35 @@
 # git config for current user.
 vim ~/.gitconfig
-
 # set a config value
 git config --global http.proxy http://127.0.0.1:12333
+
+# initialize a git repo in specified path(it is a subdirectory of current folder), `.` by default.
+git init [directory]
 
 ## commit
 # alter the message of last commit
 git commit --amend -m "message"
 
+# apply changes this commit made compared to its parent commit to current branch.
+# it's useful when you just need some modifications but not all(use git merge instead).
+git cherry-pick [commit_id]
 
 ## branch
 # list all branches
 git branch -a
-
 # create a branch
 git branch [name]
-
 # create a branch from specified commit or branch. from current branch by default.
 git checkout -b [name] [commit_id||branch_name]
-
 # delete local branch, -D for force delete.
 git branch -d [name]
 git branch -D [name]
-
 # delete remote branch
 git push origin --delete [name]
-
 # link local branch to remote branch.
 git branch --set-upstream-to=[remote_branch] [local_branch]
-
 # change git repository
 git remote set-url origin https://github.com/szq-123/codingPractice.git
-
 
 ## git reset. reset the `head` pointer and the branch pointer that `head` is pointing at.
 # reset local repo.
@@ -41,6 +39,12 @@ git reset --mixed [commit_id]
 # reset local repo, 暂存区 and workspace.
 git reset --hard [commit_id]
 
+# cancel tracing a file.
+git rm -f --cached [file]
+
+## log
+# show commit history for current branch.
+git log
 
 # list traced files in current folder. -O for untracked.
 # https://blog.csdn.net/ystyaoshengting/article/details/104029519
