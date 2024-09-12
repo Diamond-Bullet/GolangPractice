d884bd492bcd5efef9cea095773eb651825a167a
royalsong <royalsong@futunn.com>
2024-09-12 16:30:45 +0800

    feat: update

--- utils/stack_error/error.go
+++ utils/stack_error/error.go
@@ -1,40 +1,76 @@
 package stack_error
 
-import "fmt"
+import (
+	"GolangPractice/utils/logger"
+	"fmt"
+)
 
-type StackError interface {
+type ErrorType struct {
+	Code       int
+	MetricName string
+}
+
+var (
+	InternalError = ErrorType{
+		Code:       10000,
+		MetricName: "InternalError",
+	}
+)
+
+type Error interface {
 	Error() string
 	ErrorCode() int
 	ErrorMsg() string
-	String() string
 }
 
-type stackError struct {
+type StackError struct {
 	Code    int
 	Message string
 	*stack
 }
 
-func New(errCode int, message string) StackError {
-	return &stackError{
+func New(errCode int, message string) Error {
+	return &StackError{
 		Code:    errCode,
 		Message: message,
 		stack:   callers(),
 	}
 }
 
-func (s *stackError) Error() string {
+func (s *StackError) Error() string {
 	return fmt.Sprintf("code: %d, message: %s, stack: %+v", s.Code, s.Message, s.stack)
 }
 
-func (s *stackError) ErrorCode() int {
+func (s *StackError) ErrorCode() int {
 	return s.Code
 }
 
-func (s *stackError) ErrorMsg() string {
+func (s *StackError) ErrorMsg() string {
 	return s.Message
 }
 
-func (s *stackError) String() string {
-	return s.Error()
+func NewWithLog(errCode int, message string) Error {
+	stackError := &StackError{
+		Code:    errCode,
+		Message: message,
+		stack:   callers(),
+	}
+	logger.Errorln(stackError.Error())
+	return stackError
+}
+
+func NewWithLogMetric(errType ErrorType, format string, v ...any) Error {
+	message := fmt.Sprint(format, v)
+
+	stackError := &StackError{
+		Code:    errType.Code,
+		Message: message,
+		stack:   callers(),
+	}
+
+	logger.Errorln(message, " stack: ", stackError.Error())
+
+	// TODO metric package
+
+	return stackError
 }
