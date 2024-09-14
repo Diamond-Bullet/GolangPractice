fd03df29d3a3ede193ca619877045833e557c012
royalsong <royalsong@futunn.com>
2024-09-14 18:21:56 +0800

    feat: design pattern

---
+++ golang/coding/design_pattern/Builder_Factory_test.go
@@ -0,0 +1,86 @@
+package design_pattern
+
+import (
+	"fmt"
+	"testing"
+)
+
+/*
+建造者模式
+
+将一个复杂对象的构建与它的表示分离，使得同样的构建过程可以创建不同的表示。
+
+复杂对象相当于一辆有待建造的汽车，而对象的属性相当于汽车的部件，建造产品的过程就相当于组合部件的过程。
+由于组合部件的过程很复杂，因此，这些部件的组合过程往往被“外部化”到一个称作建造者的对象里，
+建造者返还给客户端的是一个已经建造完毕的完整产品对象，而用户无须关心该对象所包含的属性以及它们的组装方式，这就是建造者模式的模式动机。
+
+
+使用场景：
+	当一个类的构造函数参数个数超过4个，而且这些参数有些是可选的参数，考虑使用构造者模式。
+*/
+
+func TestBuilder(t *testing.T) {
+	mac := NewMac("Intel Core i7-10900", "Santax 16GB DDR4")
+	director := NewDirector(mac)
+	director.Construct("MAC Standard KeyBoard", "SkyWorth 15.6", 6)
+	println(mac.GetComputer())
+}
+
+// Builder 生成器接口
+type Builder interface {
+	PartUsbCount(int)
+	PartKeyBoard(string)
+	PartDisplay(string)
+}
+
+type MacBuilder struct {
+	CPU, RAM          string
+	USBCount          int
+	KeyBoard, Display string
+}
+
+func NewMac(cpu, ram string) *MacBuilder {
+	return &MacBuilder{
+		CPU: cpu,
+		RAM: ram,
+	}
+}
+
+func (m *MacBuilder) PartUsbCount(usbCount int) {
+	m.USBCount = usbCount
+}
+
+func (m *MacBuilder) PartKeyBoard(keyBoard string) {
+	m.KeyBoard = keyBoard
+}
+
+func (m *MacBuilder) PartDisplay(display string) {
+	m.Display = display
+}
+
+func (m MacBuilder) GetComputer() string {
+	return fmt.Sprintf(`
+	CPU:		%s
+	RAM:		%s
+	USBCOUNT:	%d
+	KEYBOARD:	%s
+	DISPLAY:	%s
+`, m.CPU, m.RAM, m.USBCount, m.KeyBoard, m.Display)
+}
+
+// Director 指导器，负责组装各部分
+type Director struct {
+	builder Builder
+}
+
+func (d *Director) Construct(keyBoard, DisPlay string, UsbCount int) {
+	d.builder.PartKeyBoard(keyBoard)
+	d.builder.PartDisplay(DisPlay)
+	d.builder.PartUsbCount(UsbCount)
+}
+
+func NewDirector(builder Builder) *Director {
+	return &Director{
+		builder: builder,
+	}
+}
---
+++ golang/coding/design_pattern/Object_Pool_test.go
@@ -0,0 +1,90 @@
+package design_pattern
+
+/*
+	对象池模式
+*/
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestObjectPool(t *testing.T) {
+	pool := NewPool(10)
+	go pool.Run()
+
+	for i := 0; i < 40; i++ {
+		pool.Wg.Add(1)
+		pool.PushTask(&Task{ID: i, work: func(i ...interface{}) error {
+			time.Sleep(time.Second)
+			return nil
+		}})
+	}
+
+	pool.Wg.Wait()
+	fmt.Println("goroutines: ", runtime.NumGoroutine())
+	pool.Stop()
+	time.Sleep(time.Second)
+	fmt.Println("goroutines: ", runtime.NumGoroutine())
+}
+
+type Task struct {
+	ID   int
+	work func(...interface{}) error
+}
+
+func (t *Task) Execute() error {
+	return t.work()
+}
+
+func NewTask(work func(...interface{}) error, ID int) *Task {
+	return &Task{
+		ID:   ID,
+		work: work,
+	}
+}
+
+// Pool 对象池
+type Pool struct {
+	WorkerNum   int
+	JobsChannel chan *Task
+	Wg          *sync.WaitGroup
+}
+
+func NewPool(workerNum int) *Pool {
+	return &Pool{
+		WorkerNum:   workerNum,
+		JobsChannel: make(chan *Task, workerNum),
+		Wg:          &sync.WaitGroup{},
+	}
+}
+
+func (p *Pool) PushTask(task *Task) {
+	p.JobsChannel <- task
+}
+
+func (p *Pool) Work(workerID int) {
+	for task := range p.JobsChannel {
+		err := task.Execute()
+		if err != nil {
+			fmt.Println(workerID, "run error:", err)
+		} else {
+			fmt.Println(workerID, "run success!")
+		}
+
+		p.Wg.Done()
+	}
+}
+
+func (p *Pool) Run() {
+	for i := 0; i < p.WorkerNum; i++ {
+		go p.Work(i)
+	}
+}
+
+func (p *Pool) Stop() {
+	close(p.JobsChannel)
+}
